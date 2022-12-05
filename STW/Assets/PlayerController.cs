8b6228d19d1f678caebe33e55743421658802b20
RioBeef <mail@lennartkappes.de>
2022-12-05 20:30:44 +0100

    Object collision, Player controller and animation

---
+++ STW/Assets/PlayerController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+  public float moveSpeed = 1f;
+  public float collisionOffset = 0.05f;
+  public ContactFilter2D movementFilter;
+
+
+  Vector2 movementInput;
+  Rigidbody2D rb;
+  Animator animator;
+
+  List<RaycastHit2D> castCollision = new List<RaycastHit2D>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    rb = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+    }
+
+
+  private void FixedUpdate()
+  {
+    if(movementInput != Vector2.zero)
+    {
+      bool success = TryMove(movementInput);
+
+      if(!success)
+      {
+        success = TryMove(new Vector2(movementInput.x, 0));
+
+        if(!success)
+        {
+          success = TryMove(new Vector2(0, movementInput.y));
+        }
+      }
+      animator.SetBool("isMoving", success);
+    } else
+    {
+      animator.SetBool("isMoving", false);
+    }
+  }
+
+  private bool TryMove(Vector2 direction)
+  {
+    int count = rb.Cast(
+        direction,
+        movementFilter,
+        castCollision,
+        moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+    if (count == 0)
+    {
+      rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+      return true;
+    } else
+    {
+      return false;
+    }
+  }
+
+  void OnMove(InputValue movementValue)
+  {
+    movementInput = movementValue.Get<Vector2>();
+  }
+}
